50a38a8ff5c4b837717ac053184bed3ddba02acf
Arisson-Oliveira <arissom.oliveira@gmail.com>
2024-09-26 20:35:33 -0300

    teste 1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from flask import Flask
 
-                                                                                                                                                                                        
+# Inicialização da Função
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return 'Hello comunity'
+
+if __name__ == '__main__':
+    app.run(debug=True)
