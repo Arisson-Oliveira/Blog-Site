b044373f2013ff45bbb43385e3da0851e3eaf841
Arisson-Oliveira <arissom.oliveira@gmail.com>
2024-09-26 20:54:43 -0300

    arquivos html

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 # Inicialização da Função
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return 'Hello comunity'
+    return render_template('home.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
