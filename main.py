1585533890b9ff950542a46b55565aa42fb8d201
Arisson-Oliveira <arissom.oliveira@gmail.com>
2024-09-28 11:51:07 -0300

    paginas e navbar

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 # Inicialização da Função
 app = Flask(__name__)
+
+lista_usuarios = ['Arisson', 'Welisson']
 
 @app.route('/')
 def homepage():
@@ -11,6 +13,16 @@ def homepage():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login-conta')
+def loginconta():
+    return render_template('login-conta.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
