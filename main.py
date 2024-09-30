92008a8e2454c76859340005cc03ed1b5960f88e
Arisson-Oliveira <arissom.oliveira@gmail.com>
2024-09-30 20:31:12 -0300

    importacao banco de dados

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from forms import FormLogin, FormCriarConta
+from flask_sqlalchemy import SQLAlchemy
 
 # Inicialização da Função
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dfvbusvbdbcsubcxbufgefdg87dftrg67df'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
-lista_usuarios = ['Arisson', 'Welisson']
+database = SQLAlchemy(app)
 
 @app.route('/')
 def homepage():
